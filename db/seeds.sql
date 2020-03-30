ee777f612d12bb5f7d494f0a7023e1d718dc37b1
lalliere <emilylallier@yahoo.com>
2020-03-30 18:08:41 -0400

    intial commit - database + seeds

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken & Avocado BLT", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ BACON MEAT SPECIAL", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Penne alla Vodka Pizza", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Cheese w/extra cheese", false);
