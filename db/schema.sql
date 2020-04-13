54ebacce20e21c3bcc7a6a05a31a9dc7203eba2d
lalliere <emilylallier@yahoo.com>
2020-04-13 18:28:32 -0400

    column name changes

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT (11) AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    name VARCHAR(255) NOT NULL,
+    eaten BOOLEAN DEFAULT false,
 
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken & Avocado BLT", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ MEAT SPECIAL", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Penne alla Vodka Pizza", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Five Cheese w/Extra Cheese", false);
+INSERT INTO burgers (name, eaten) VALUES ("Chicken & Avocado BLT", false);
+INSERT INTO burgers (name, eaten) VALUES ("BBQ MEAT SPECIAL", false);
+INSERT INTO burgers (name, eaten) VALUES ("Penne alla Vodka Pizza", false);
+INSERT INTO burgers (name, eaten) VALUES ("Five Cheese w/Extra Cheese", false);
