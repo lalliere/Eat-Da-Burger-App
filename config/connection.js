50c55cb2342ff429bedd31b958f0f6c1e53f4a4b
lalliere <emilylallier@yahoo.com>
2020-04-19 10:02:46 -0400

    updated connection.js to use env variables

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,21 @@
+require('dotenv').config()
 const mysql = require("mysql");
-// Set up connection information
-let connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Jennings22!",
-  database: "burgers_db"
-});
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: process.env.DB_PASSWORD,
-//     database: 'burgers_db'
-//   });
-// };
+// Set up connection information
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    dialect: 'mysql',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+};
 
 // Connect to the database
 connection.connect(function(err) {
