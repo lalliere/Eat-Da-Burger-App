6ab7a4a058fa0f1d8b75bfc781c1db7a6baec295
lalliere <emilylallier@yahoo.com>
2020-04-19 16:10:13 -0400

    saving

--- models/burger.js
+++ models/burger.js
@@ -4,33 +4,26 @@ const orm = require("../config/orm.js");
 
 const burger = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.all(function(res) {
       cb(res);
     });
   },
 
   // The variables cols and vals are arrays.
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "name", "eaten"
-    ], [
-      name, false
-    ], function(res) {
+  create: function(cols, vals, cb) {
+    orm.create(cols, vals, function(res) {
       cb(res);
     });
   },
 
-  update: function(id, cb) {
-    let condition = "id=" + id;
-    orm.update("burgers", {
-      eaten: true
-    }, condition, function(res) {
+  update: function(objColVals, condition, cb) {
+    orm.update(objColVals, condition, function(res) {
       cb(res);
     });
   },
 
   delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.delete(condition, function(res) {
       cb(res);
     });
   }
