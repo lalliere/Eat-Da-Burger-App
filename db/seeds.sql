0bdc8a028b6be0033b68b2d8906e5772755a9168
lalliere <emilylallier@yahoo.com>
2020-04-19 09:56:20 -0400

    made one more burger be already eaten

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (name, eaten) VALUES ("Chicken & Avocado BLT", false);
 INSERT INTO burgers (name, eaten) VALUES ("BBQ MEAT SPECIAL", false);
-INSERT INTO burgers (name, eaten) VALUES ("Penne alla Vodka Pizza", false);
-INSERT INTO burgers (name, eaten) VALUES ("Five Cheese w/Extra Cheese", false);
+INSERT INTO burgers (name, eaten) VALUES ("Penne alla Vodka Pizza", true);
+INSERT INTO burgers (name, eaten) VALUES ("Five Cheese w/Extra Cheese", true);
