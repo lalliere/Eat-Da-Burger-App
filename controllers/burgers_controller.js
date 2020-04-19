8052e2acedb658b4c146bb868a92611cba92faf0
lalliere <emilylallier@yahoo.com>
2020-04-19 16:17:12 -0400

    saving

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,35 +4,41 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-  res.redirect('/burgers');
-});
 
-router.get("/burgers", function (req, res) {
+router.get("/", function (req, res) {
+  
   burger.all(function (data) {
     let burgObj = {
       burgers: data
     };
     
+    console.log(burgObj);
     res.render("index", burgObj);
   });
 })
 
 router.post("/api/burgers", function (req, res) {
-  burger.create(req.body.name, function (result) {
+  burger.create([
+    "name", 
+    "eaten"
+  ], 
+  [
+    req.body.name, 
+    req.body.eaten
+  ], function (result) {
       // Send back the ID of the new burger
       res.json({ id: result.insertId });
     });
 });
 
 router.put("/api/burgers/:id", function (req, res) {
+  let condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  burger.update(
-    {
-      eaten: req.body.eaten
-    }, condition, function (result) {
+  burger.update({
+    eaten: 1
+  }, condition, function (result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
