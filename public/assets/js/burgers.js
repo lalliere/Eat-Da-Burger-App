27c544eea5a698e538dba1d44abf629cdd9e68de
lalliere <emilylallier@yahoo.com>
2020-04-25 15:37:17 -0400

    updated file structure

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,30 +2,38 @@
 
 $(document).ready(function () {
 
-  $(".eat-burger").on("click", function (event) {
-    let id = $(this).data("id");
+  $(".change-status").on("click", function () {
+    console.log("testing")
+    // let id = $(this).data("id");
+    // let newState = $(this).data("newStatus");
+
+    // let newEatState = {
+    //   eaten = newState
+    // };
     
-    let eaten = 1;
-    
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: eaten
-    }).then(
-      function () {
-        console.log("Changed state of being consumed to devoured");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    // // Send the PUT request.
+    // $.ajax("/api/burgers/" + id, {
+    //   type: "PUT",
+    //   data: newEatState
+    // }).then(
+    //   function () {
+    //     console.log("Changed state of being consumed to", newState);
+    //     // Reload the page to get the updated list
+    //     location.reload();
+    //   }
+    // );
   });
 
   $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
+    console.log("test");
+
+
     let newBurger = {
-      name: $("#burgName").val().trim()
+      name: $("#burger").val().trim(),
+      eaten: 0
     };
 
     // Send the POST request.
@@ -35,7 +43,7 @@ $(document).ready(function () {
     }).then(
       function () {
         // M.toast({html: "Burger Added!"});
-        console.log("Created a new burger!");
+        console.log("Created new burger!");
         // Reload the page to get the updated list
         location.reload();
       },
@@ -49,8 +57,7 @@ $(document).ready(function () {
 
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
-      type: "DELETE",
-      data: id
+      type: "DELETE"
     }).then(
       function () {
         console.log("Deleted burger", id);
