b462611e524897b740f76e71c9c995624478ba69
lalliere <emilylallier@yahoo.com>
2020-04-25 15:39:09 -0400

    burger.update: eaten: req.body.eaten instead of 0;

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -36,9 +36,10 @@ router.put("/api/burgers/:id", function (req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
-    eaten: 1
-  }, condition, function (result) {
+  burger.update(
+    {
+    eaten: req.body.eaten
+    }, condition, function (result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
