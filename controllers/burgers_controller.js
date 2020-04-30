dfecdaa4b5a91bb17b2af1df4eb0c6e24db8847f
lalliere <emilylallier@yahoo.com>
2020-04-29 22:36:05 -0400

    added reorder button; reworked js ('put' methods)

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,15 +31,17 @@ router.post("/api/burgers", function (req, res) {
     });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-  let condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update(
     {
-    eaten: true
-    }, condition, function (result) {
+      eaten: req.body.eaten
+    },
+    condition,
+    function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,23 +1,27 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 
 $(function () {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEat = $(this).data("neweat");
 
-  $(".eat-burger").on("click", function (event) {
-    // console.log("testing")
-    let id = $(this).data("id");
-    let eaten = 0;
+    var newEatState = {
+      eaten: !newEat
+    };
+
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: eaten
+      data: newEatState
     }).then(
-      function () {
-        console.log("The burger has been devoured");
+      function() {
+        console.log("changed eat to", newEat);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
+
 
   $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
